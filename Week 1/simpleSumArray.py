e418feedbab4a873b0fd0f7c61f1a859f3601a56
Kuria K. Ken <57272683+ElevnthKuria@users.noreply.github.com>
2022-06-23 12:25:55 +0300

    Create simpleSumArray.py

---
+++ Week 1/simpleSumArray.py
@@ -0,0 +1,8 @@
+#https://www.hackerrank.com/challenges/simple-array-sum/problem
+
+def simpleArraySum(ar):
+    # Write your code here
+    sum_all = 0
+    for i in ar:
+        sum_all += i
+    return sum_all
